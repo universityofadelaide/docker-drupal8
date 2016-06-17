a64ef4fb68a05546a3e729dc753d4a8b2f0070b4
Simon Lindsay <simon.lindsay@adelaide.edu.au>
2016-06-17 14:37:42 +0930

    Support specifying a user on the command line, call the entry script to set variables etc instead of bash directly

--- dsh.sh
+++ dsh.sh
@@ -2,15 +2,18 @@
 
 . common.sh
 
+SHELL_USER=${1:-${USER}}
+
 if [ ! -z "${SSH_AUTH_SOCK}" ]; then
     SOCK=$(dirname ${SSH_AUTH_SOCK})
     set +e
     docker run -it -w /code -h utility --name ${PROJECT}_utility \
+           -e USER=${SHELL_USER} \
            -v ${SOCK}:${SOCK} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
            -v ${DIR}:/code \
-           -v ${DIR}/.composer:/root/.composer \
+           -v ${DIR}/.composer:/${SHELL_USER}/.composer \
            --net=${PROJECT}_default \
-           uofa/docker_drupal_utility /bin/bash
+           uofa/docker_drupal_utility /entry.sh
     set -e
 
     docker rm ${PROJECT}_utility
